dbba82d9349f6d5f88ec9ccadadec5d2c439f5cf
Samuel Villa <Samuelsnc1@gmail.com>
2021-03-02 08:19:05 -0600

    ERRORES

---
+++ src/Parcial1/Celular.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Parcial1;
+
+/**
+ *
+ * @author chamo_000
+ */
+public class Celular {
+    //Atributos
+    //sasjgdjsa Crear variables
+    //modificador de acceso tipo nombre
+    //Esto es una Clase!
+    
+    //Contexto
+    
+    //Variables Globales
+    
+        private String marca;
+        private String capacidad;
+        private String tamaño;
+        private String color;
+        private double precio;
+        private String modelo;
+        private boolean chino;
+        private String pais;
+        
+        
+ public Celular(){
+     System.out.println("");
+ }   
+ //ModaAcces tipo Retorno Nombre(tipoDato NombreParam)
+ //Parametros => Variable Locales 
+ public void Llamar(int numero){
+     //Variable Local
+     //COntexto Reducido
+     
+     
+ }
+ public void Colgar(){
+}
+
+
